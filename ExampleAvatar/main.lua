4a9dcf47baa958db82c8b8fec1f4e084f190d8e5
XanderCreates <69139978+XanderCreates@users.noreply.github.com>
2025-04-28 21:07:21 -0400

    fix fix

--- ExampleAvatar/main.lua
+++ ExampleAvatar/main.lua
@@ -1,5 +1,5 @@
 vanilla_model.PLAYER:setVisible(false)
 
 local cAPI = require("just-lean")
-local torso = cAPI.lean:new(models.model.root.Torso, {x= -45, y= -15}, {x=45,y=15}, 0.4625, true)
+local torso = cAPI.lean:new(models.model.root.Torso, {x= -45, y= -15}, {x=45,y=15}, 0.4625, true, true)
 local head = cAPI.head:new(models.model.root.Torso.Head, 0.3, 1, true, true) --optional
