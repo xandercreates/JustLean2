82259ce6990f06b0efea06b7307c4910d96f8fd8
XanderCreates <69139978+XanderCreates@users.noreply.github.com>
2025-04-28 21:41:50 -0400

    Update main.lua

--- ExampleAvatar/main.lua
+++ ExampleAvatar/main.lua
@@ -2,17 +2,19 @@ vanilla_model.PLAYER:setVisible(false)
 
 local cAPI = require("just-lean")
 local torso = cAPI.lean:new(
-    models.model.root.Torso,  --ModelPart, change this accordingly
-    { x = -45, y = -15 },     -- minimum Lean, can be either a table or Vector2. Change to suit your needs
-    { x = 45, y = 15 },       -- maximum Lean, can be either a table or Vector2. Change to suit your needs.
-    0.4625,                   --speed,
-    true,                     --optional idle breathing motion
-    true                      --enabled or not
+    models.model.root.Torso,      --ModelPart, change this accordingly
+    { x = -45, y = -15 },         -- minimum Lean, can be either a table or Vector2. Change to suit your needs
+    { x = 45, y = 15 },           -- maximum Lean, can be either a table or Vector2. Change to suit your needs.
+    0.4,                          --speed,
+    "inOutElastic",                 --interpolation method. Takes string, Valid vals: "linear", "inOutSine", "inOutCubic", "inOutElastic"
+    true,                         --optional breathing idle
+    true                          --enabled or not
 )
-local head = cAPI.head:new(   --optional
+local head = cAPI.head:new(       --optional
     models.model.root.Torso.Head, --ModelPart, change this accordingly
-    0.3,                      --speed
-    1,                        --Tilt. The higher the less
-    true,                     --Rotate Vanilla Head Instead (Will rotate modelpart if it follows vanilla head)
-    true                      --enabled or not
+    0.6,                          --speed
+    1,                            --Tilt. The higher the less
+    "inOutCubic",                           --interpolation method. Takes string, Valid vals: "linear", "inOutSine", "inOutCubic", "inOutElastic"
+    true,                         --Rotate Vanilla Head Instead (Will rotate modelpart if it follows vanilla head)
+    true                          --enabled or not
 )
