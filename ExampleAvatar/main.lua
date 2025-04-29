8715dff38eb4e368ce1e20191d61b1f6147839c5
XanderCreates <69139978+XanderCreates@users.noreply.github.com>
2025-04-28 21:15:19 -0400

    Update main.lua

--- ExampleAvatar/main.lua
+++ ExampleAvatar/main.lua
@@ -1,5 +1,18 @@
 vanilla_model.PLAYER:setVisible(false)
 
 local cAPI = require("just-lean")
-local torso = cAPI.lean:new(models.model.root.Torso, {x= -45, y= -15}, {x=45,y=15}, 0.4625, true, true)
-local head = cAPI.head:new(models.model.root.Torso.Head, 0.3, 1, true, true) --optional
+local torso = cAPI.lean:new(
+    models.model.root.Torso,  --ModelPart, change this accordingly
+    { x = -45, y = -15 },     -- minimum Lean, can be either a table or Vector2. Change to suit your needs
+    { x = 45, y = 15 },       -- maximum Lean, can be either a table or Vector2. Change to suit your needs.
+    0.4625,                   --speed,
+    true,                     --optional idle breathing motion
+    true                      --enabled or not
+)
+local head = cAPI.head:new(   --optional
+    models.model.root.Torso.Head, --ModelPart, change this accordingly
+    0.3,                      --speed
+    1,                        --Tilt. The higher the less
+    true,                     --Rotate Vanilla Head Instead (Will rotate modelpart if it follows vanilla head)
+    true                      --enabled or not
+)
