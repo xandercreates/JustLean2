b6aaf1ebc4bf5f0fc7c079f619fba8275aa64fff
XanderCreates <69139978+XanderCreates@users.noreply.github.com>
2025-05-04 17:02:41 -0400

    include influence example

--- ExampleAvatar/main.lua
+++ ExampleAvatar/main.lua
@@ -6,7 +6,7 @@ local torso = cAPI.lean:new(
     { x = -45, y = -15 },         -- minimum Lean, can be either a table or Vector2. Change to suit your needs
     { x = 45, y = 15 },           -- maximum Lean, can be either a table or Vector2. Change to suit your needs.
     0.4,                          --speed,
-    "inOutElastic",                 --interpolation method. Takes string, Valid vals: "linear", "inOutSine", "inOutCubic", "inOutElastic"
+    "inOutElastic",               --interpolation method. Takes string, Valid vals: "inOutSine", "inOutCubic", "linear"
     true,                         --optional breathing idle
     true                          --enabled or not
 )
@@ -14,7 +14,23 @@ local head = cAPI.head:new(       --optional
     models.model.root.Torso.Head, --ModelPart, change this accordingly
     0.6,                          --speed
     1,                            --Tilt. The higher the less
-    "inOutCubic",                           --interpolation method. Takes string, Valid vals: "linear", "inOutSine", "inOutCubic", "inOutElastic"
+    "inOutCubic",                 --interpolation method. Takes string, Valid vals: "inOutSine", "inOutCubic", "linear"
     true,                         --Rotate Vanilla Head Instead (Will rotate modelpart if it follows vanilla head)
     true                          --enabled or not
+)
+local leftarm = cAPI.influence:new(
+    models.model.root.Torso.LeftArm,
+    0.5,         --speed
+    "inOutSine", --interpolation method. Takes string, Valid vals: "inOutSine", "inOutCubic", "linear"
+    { 1, 1, 1 }, --how much you want the part to be influenced (1 or 3 values allowed)
+    torso,       --give it a metatable
+    true         --enabled or not
+)
+local rightarm = cAPI.influence:new(
+    models.model.root.Torso.RightArm,
+    0.5,         --speed
+    "inOutSine", --interpolation method. Takes string, Valid vals: "inOutSine", "inOutCubic", "linear"
+    { 1, 1, 1 }, --how much you want the part to be influenced (1 or 3 values allowed)
+    torso,       --give it a metatable
+    true         --enabled or not
 )
