8863f1a90ab9883cb323faae28c3926e0dca2bbc
XanderCreates <69139978+XanderCreates@users.noreply.github.com>
2025-05-04 16:57:18 -0400

    added influence

--- just-lean.lua
+++ just-lean.lua
@@ -2,6 +2,11 @@
 Rewrite Structure Heavily Inspired by Squishy's API
 ]]                                                       --
 --#region 'SquishyChecker'
+---@param message string
+---@param level number ?
+---@param prefix string ?
+---@param toLog boolean ?
+---@param both boolean ?
 local function warn(message, level, prefix, toLog, both) --by Auria, Modified by Xander
     local _, traceback = pcall(function() error(message, (level or 1) + 3) end)
     if both or not toLog then
@@ -43,7 +48,6 @@ function events.tick()
 end
 
 --#endregion
-
 --#region 'Math Extras'
 ---@diagnostic disable
 
@@ -51,43 +55,59 @@ end
 local easings = {}
 
 ---@private
----@param a number|Vector|Matrix
----@param b number|Vector|Matrix
+---@param a number|Vector.any|Matrix.any?
+---@param b number|Vector.any|Matrix.any?
 ---@param t number
----@return number|Matrix|Vector
+---@return number|Vector2|Vector3|Vector4|Matrix?
 function easings.inOutSine(a, b, t)
     return math.map(-(math.cos(math.pi * t) - 1) / 2, 0, 1, a, b)
 end
 
 ---@private
----@param a number|Vector|Matrix
----@param b number|Vector|Matrix
+---@param a number|Vector2|Vector3|Vector4|Matrix?
+---@param b number|Vector2|Vector3|Vector4|Matrix?
 ---@param t number
----@return number|Matrix|Vector
+---@return number|Vector2|Vector3|Vector4|Matrix?
 function easings.inOutCubic(a, b, t)
     local v = t < 0.5 and 4 * t ^ 3 or 1 - (-2 * t + 2) ^ 3 / 2
     return math.map(v, 0, 1, a, b)
 end
 
-function easings.linear(a, b, t) --literally math.lerp
+function easings.inOutElastic(a, b, x)
+    local t = x + 0.05
+    local c5 = (2 * math.pi) / 4.5
+    local v
+    if x == 0 then
+        v = 0
+    elseif x == 1 then
+        v = 1
+    elseif x < 0.5 then
+        v = -((2 ^ (20 * t - 10)) * math.sin((20 * t - 11.125) * c5)) / 2
+    else
+        v = ((2 ^ (-20 * t + 10)) * math.sin((20 *  t - 11.125) * c5)) / 2 + 1
+    end
+    return math.map(v, 0, 1, a, b)
+end
+
+function easings.linear(a, b, t)
     return a + (b - a) * t
 end
 
 ---@private
----@param a number|Vector|Matrix
----@param b number|Vector|Matrix
+---@param a number|Vector2|Vector3|Vector4|Matrix?
+---@param b number|Vector2|Vector3|Vector4|Matrix?
 ---@param t number
 ---@param s string
----@return number|Matrix|Vector
+---@return number|Vector2|Vector3|Vector4|Matrix?
 local function ease(a, b, t, s)
     return easings[s](a, b, t)
 end
 
 ---@private
----@param val number|Matrix|Vector
----@param min number|Matrix|Vector
----@param max number|Matrix|Vector
----@return number|Matrix|Vector
+---@param val number|Vector2|Vector3|Vector4|Matrix?
+---@param min number|Vector2|Vector3|Vector4|Matrix?
+---@param max number|Vector2|Vector3|Vector4|Matrix?
+---@return number|Vector2|Vector3|Vector4|Matrix?
 local function clamp(val, min, max)
     return math.min(math.max(val, min), max)
 end
@@ -116,24 +136,72 @@ local abs = math.abs
 --#region 'Just-Lean'
 ---@class cratesAPI
 local cratesAPI = {}
+cratesAPI.__index = cratesAPI
 cratesAPI.allowAutoUpdates = true
 cratesAPI.enabled = true
 cratesAPI.debug = false
-cratesAPI.exposeEasing = false
+cratesAPI.exposeEasing = true
 
+function cratesAPI:enable()
+    self.enabled = true
+    return self
+end
 
----@class Lean
-cratesAPI.lean = {}
-cratesAPI.lean.__index = cratesAPI.lean
-cratesAPI.lean.activeLeaning = {}
+function cratesAPI:disable(x)
+    self.enabled = false
+    return self
+end
+
+function cratesAPI:toggle(x)
+    if x == nil then
+        self.enabled = not self.enabled
+    elseif x ~= nil then
+        self.enabled = x
+    end
+    if self.enabled == false then
+        self:disable(x)
+    end
+    return self
+end
+
+function cratesAPI:add(...)
+    local vals = { ... }
+    --log(vals)
+    if #vals == 0 then
+        self.offset:reset()
+        return self
+    elseif type(vals[1]) == "Vector3" then
+        self.offset:add(vals[1])
+        return self
+    elseif (not type(vals[1]) == "Vector3") and type(vals[1]) == "number" then
+        local vx, vy, vz = table.unpack(vals)
+        --log(vx,vy,vz)
+        self.offset:add(vx,vy,vz)
+        return self
+    else
+        return self
+       -- error("Expected Vector3 or Numbers")
+    end
+end
+
+function cratesAPI:getRot()
+    return self._rot
+end
+
+---@class lean
+lean = {}
+lean.__index = lean
+setmetatable(lean, cratesAPI)
+lean.activeLeaning = {}
 ---@param modelpart ModelPart
----@param minLean Vector2 | table
----@param maxLean Vector2 | table
+---@param minLean Vector2 | table?
+---@param maxLean Vector2 | table?
 ---@param speed number
 ---@param interp string
 ---@param enabled boolean
-function cratesAPI.lean.new(self, modelpart, minLean, maxLean, speed, interp, breathing, enabled)
-    local self = setmetatable({}, cratesAPI.lean)
+---@return lean
+function lean.new(self, modelpart, minLean, maxLean, speed, interp, breathing, enabled)
+    local self = setmetatable({}, lean)
     self.modelpart = modelpart
     if type(minLean) == "table" then
         self.minLean = vec(minLean.x or minLean[1], minLean.y or minLean[2]) or vec(-45, -15)
@@ -153,39 +221,10 @@ function cratesAPI.lean.new(self, modelpart, minLean, maxLean, speed, interp, br
     self.interp = interp
     self.offset = vec(0, 0, 0)
 
-    ---@vararg number|Vector3
-    function self:add(...)
-        local vals = { ... }
-        if #vals == 0 then
-            return self.offset:reset()
-        elseif type(vals[1]) == "Vector3" then
-            return self.offset:add(vals[1])
-        elseif not type(vals[1]) == "Vector3" then
-            return self.offset:add(table.unpack(vals))
-        else
-            error("Expected Vector3 or Numbers")
-        end
-    end
 
     function self:reset()
         self.offset:reset()
         self.rot:reset()
-        return self
-    end
-
-    function self:toggle(x)
-        if x == nil then
-            self.enabled = not self.enabled
-        elseif x ~= nil then
-            self.enabled = x
-        end
-        if self.enabled == false then
-            self:disable(x)
-        end
-    end
-
-    function self:enable()
-        self.enabled = true
         return self
     end
 
@@ -198,25 +237,26 @@ function cratesAPI.lean.new(self, modelpart, minLean, maxLean, speed, interp, br
         return self
     end
 
-    table.insert(cratesAPI.lean.activeLeaning, self)
+    table.insert(lean.activeLeaning, self)
     return self
 end
 
----@class Head
-cratesAPI.head = {}
-cratesAPI.head.__index = cratesAPI.head
-cratesAPI.head.activeHead = {}
-
----comment
----@param self Head
+---@class head
+head = {}
+head.__index = head
+setmetatable(head, cratesAPI)
+head.activeHead = {}
+---@param self head
 ---@param modelpart ModelPart
 ---@param speed number
 ---@param tilt number
+---@param interp string
 ---@param vanillaHead boolean
 ---@param enabled boolean
----@return Head
-function cratesAPI.head.new(self, modelpart, speed, tilt, interp, vanillaHead, enabled)
-    local self = setmetatable({}, cratesAPI.head)
+---@return head
+function head.new(self, modelpart, speed, tilt, interp, vanillaHead, enabled)
+    local self = setmetatable({}, head)
+    ---@class head
     self.modelpart = modelpart
     self.enabled = enabled or true
     self.speed = speed or 0.3625
@@ -225,21 +265,6 @@ function cratesAPI.head.new(self, modelpart, speed, tilt, interp, vanillaHead, e
     self.rot = vec(0, 0, 0)
     self.tilt = (1 / (tilt or 4))
     self.interp = interp or "inOutSine"
-
-    function self:toggle(x)
-        if x == nil then
-            self.enabled = not self.enabled
-        elseif x ~= nil then
-            self.enabled = x
-        end
-        if self.enabled == false then
-            self:disable(x)
-        end
-    end
-
-    function self:enable()
-        self.enabled = true
-    end
 
     function self:disable(x)
         if not x then
@@ -251,13 +276,59 @@ function cratesAPI.head.new(self, modelpart, speed, tilt, interp, vanillaHead, e
         return self
     end
 
-    table.insert(cratesAPI.head.activeHead, self)
+    table.insert(head.activeHead, self)
+    return self
+end
+
+
+
+---@class influence
+influence = {}
+influence.__index = influence
+setmetatable(influence, cratesAPI)
+influence.activeInfluences = {}
+---@param self influence
+---@param modelpart ModelPart
+---@param speed number
+---@param interp string
+---@param factor number|table|Vector3?
+---@param enabled boolean
+---@return influence
+function influence.new(self, modelpart, speed, interp, factor, metatable, enabled)
+    local self = setmetatable({}, influence)
+    self.modelpart = modelpart
+    self.speed = speed
+    self.interp = interp
+    self.enabled = enabled
+    self.__metatable = metatable or false
+    self.rot = self.__metatable and (-self.__metatable.modelpart:getOffsetRot()) or vec(0,0,0)
+    self._rot = self.rot
+    if type(factor) == "table" then
+        local x,y,z = table.unpack(factor)
+        self.factor = vec(x or 1,y or 1,z or 1)
+        if #factor > 3 then
+            error("Maximum Length of 3 Expected",4)
+        elseif #factor == 0 then
+            error("No Values given")
+        end
+    elseif type(factor) == "Vector3" then
+        self.factor = factor
+    elseif type(factor) == "number" then
+        self.factor = vec(factor, factor, factor)
+    else
+        self.factor = 1
+    end
+
+    table.insert(influence.activeInfluences, self)
     return self
 end
 
 --#endregion
 
 --#region 'Update'
+local hed = head.activeHead
+local le = lean.activeLeaning
+local influ = influence.activeInfluences
 function cratesAPI:avatar_init()
     if self.exposeEasing then
         math.ease = ease
@@ -265,14 +336,13 @@ function cratesAPI:avatar_init()
         math.ease = nil
     end
     if not self.enabled then return self end
-    local head = self.head.activeHead
-    local lean = self.lean.activeLeaning
-    for _, v in pairs(head) do
-        v.rot:set(((((vanilla_model.HEAD:getOriginRot()) + 180) % 360) - 180))
+
+    for _, v in pairs(hed) do
+        v.rot:set(player:getRot()-vec(0,player:getBodyYaw()).xy_)
         v._rot:set(v.rot)
     end
 
-    for _, k in pairs(lean) do
+    for _, k in pairs(le) do
         k.rot:set(vec(0, 0, 0):add(k.offset))
         k._rot:set(k.rot)
     end
@@ -281,38 +351,35 @@ end
 function cratesAPI:tick()
     if not self.enabled then return self end
     local vehicle = player:getVehicle()
-    local head = self.head.activeHead
-    local lean = self.lean.activeLeaning
-    if #lean < 1 then
+    if #le < 1 then
         if self.debug then
             warn("No Parts Specified", 4)
         end
         return false
     end
-    if #head < 1 then
+    if #hed < 1 then
         if self.debug then
-            print(
-                "Head not added/found. Creating Fallback; Will not work if you aren't using a keyworded Head part (Which follows the vanilla head!)")
+            warn("Head not added/found. Creating Fallback; Will not work if you aren't using a keyworded Head part (Which follows the vanilla head!)", 4)
         end
-        head[1] = {
+        hed[1] = {
             modelpart = vanilla_model.HEAD,
             rotScale = 1,
             vanillaHead = true,
             speed = false,
             _rot = vec(0,0,0),
             rot = vec(0,0,0),
-            enabled = true,
+            enabled = true
         }
     end
-    for id_h, v in pairs(head) do
+    for id_h, v in pairs(hed) do
         v._rot:set(v.rot)
         if v.enabled then
             v.selHead = v.modelpart or v.vanillaHead and vanilla_model.HEAD
-            for id_l, y in pairs(lean) do
+            for id_l, y in pairs(le) do
                 if id_h == id_l then --insurance
                 local player_rot = ((((player:getRot() - vec(0,player:getBodyYaw())))+180)%360)-180
                 local fpr = (-player_rot).xy_ - vec(y.rot.x, y.rot.y, -y.rot.y / 4) 
-                local final = vehicle and (((vanilla_model.HEAD:getOriginRot()+180)%360)-180) - vec(y.rot.x, y.rot.y, -y.rot.y / 4) or fpr
+                local final = vehicle and (((vanilla_model.HEAD:getOriginRot()+180)%360)-180) - vec(y.rot.x, y.rot.y, -y.rot.y / 4) or fpr        
                     v.rot:set(ease(v.rot,
                         final, v.speed or 0.5,
                         v.interp or "inOutSine"))
@@ -322,7 +389,7 @@ function cratesAPI:tick()
         end
     end
 
-    for _, k in pairs(lean) do
+    for _, k in pairs(le) do
         k._rot:set(k.rot)
         if k.enabled then
             local mainrot = vehicle and (((vanilla_model.HEAD:getOriginRot()+180)%360)-180):toRad():scale(-1,1,1) or (((((player:getRot() - vec(0,player:getBodyYaw())).xy_)+180)%360)-180):toRad()
@@ -333,7 +400,6 @@ function cratesAPI:tick()
                 (abs(cos(t)) / 16.0)
             )
             local targetVel = velmod()
-            --log(targetVel)
             local lean_x = clamp(sin(-mainrot.x / targetVel) * 45.5, k.minLean.x, k.maxLean.x)
             local lean_y = -clamp(math.sin(mainrot.y) * 45.5, k.minLean.y, k.maxLean.y)
             local rot = not player:isCrouching() and
@@ -345,27 +411,46 @@ function cratesAPI:tick()
             end
         end
     end
+    for _, l in pairs(influ) do
+        if l.enabled then
+            l._rot:set(l.rot)
+            l.rot = ease(l.rot, -l.__metatable.rot * l.factor, l.speed, l.interp or "inOutCubic")
+        end
+    end
 end
+
+cratesAPI.lean = lean
+setmetatable(cratesAPI.lean, cratesAPI)
+cratesAPI.head = head
+setmetatable(cratesAPI.head, cratesAPI)
+cratesAPI.influence = influence
+setmetatable(cratesAPI.influence, cratesAPI)
 
 function cratesAPI:render(delta)
     if not self.enabled then return self end
     if delta == 1 then return end
-    local head = self.head.activeHead
-    local lean = self.lean.activeLeaning
-    for _, v in pairs(head) do
+    
+    for _, v in pairs(hed) do
         if v.enabled then
-            local fRot = math.lerp(v._rot, v.rot, delta)
             vanilla_model.HEAD:setRot(0, 0, 0)
+            local fRot = easings.linear(v._rot, v.rot, delta)
             v.selHead:setOffsetRot(fRot)
         else
             vanilla_model.HEAD:setRot()
         end
     end
 
-    for _, k in pairs(lean) do
+    for _, k in pairs(le) do
         if k.enabled then
-            local fRot = math.lerp(k._rot, k.rot, delta)
+            local fRot = easings.linear(k._rot, k.rot, delta)
             k.modelpart:setOffsetRot(fRot)
+        end
+    end
+
+    for _, l in pairs(influ) do
+        if l.enabled then
+            local fRot = easings.linear(l._rot, l.rot, delta)
+            l.modelpart:setOffsetRot(fRot)
         end
     end
 end
