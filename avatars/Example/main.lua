a96485c8491f58f7e62cfb0ef0d3bf9dfd2b605f
xandercreates <69139978+xandercreates@users.noreply.github.com>
2025-07-24 14:56:53 -0400

    quick fix

--- avatars/Example/main.lua
+++ avatars/Example/main.lua
@@ -25,9 +25,9 @@ models.model.root.LeftLeg, --modelpart
 "linear", --interpolation
 "LEG_LEFT", --type (VALID: LEG_LEFT, LEG_RIGHT, ARM_LEFT, ARM_RIGHT) Arm types not implemented yet
 {1,0.5,0.1}, --strength
-lean, --unused
+torso, --used to grab active head or active lean modelpart rotations for use in its own rotation
 true --enabled
 )
 
 --you know the drill
-local right = jl.influence:new(models.model.root.RightLeg, 0.5, "linear", "LEG_RIGHT",{1,0.5,0.1}, lean, true)
+local right = jl.influence:new(models.model.root.RightLeg, 0.5, "linear", "LEG_RIGHT",{1,0.5,0.1}, torso, true)
