727b2cc8fbb022fa85c3fc3ddd55656fc277d791
xandercreates <69139978+xandercreates@users.noreply.github.com>
2025-07-28 03:05:33 -0400

    Update main.lua

--- avatars/Example/main.lua
+++ avatars/Example/main.lua
@@ -6,7 +6,7 @@ local torso = jl.lean:new(
     { x = -45, y = -15 },         -- minimum Lean, can be either a table or Vector2. Change to suit your needs
     { x = 45, y = 15 },           -- maximum Lean, can be either a table or Vector2. Change to suit your needs.
     0.5,                          --speed,
-    "linear",               --interpolation method. Takes string, Valid vals: "linear", "inOutSine", "inOutCubic"
+    "linear",                --interpolation method. Takes string, See just-lean.lua or ease.lua for valid interps. if not using Ease However; "linear" is the only valid interpolation
     true,                         --optional breathing idle
     true                          --enabled or not
 )
@@ -14,7 +14,7 @@ local head = jl.head:new(       --optional
     models.model.root.Torso.Head, --ModelPart, change this accordingly
     0.75,                          --speed
     0.3,                            --Tilt. the lower the less
-    "linear",                 --interpolation method. Takes string, Valid vals: "linear", "inOutSine", "inOutCubic"
+    "linear",                 --interpolation method. Takes string, See just-lean.lua or ease.lua for valid interps. if not using Ease However; "linear" is the only valid interpolation
     {1,1},
     true,                         --Rotate Vanilla Head Instead (Will rotate modelpart if it follows vanilla head)
     true                          --enabled or not
@@ -22,14 +22,14 @@ local head = jl.head:new(       --optional
 local left = jl.influence:new(
 models.model.root.LeftLeg, --modelpart
 0.5, --speed
-"linear", --interpolation
+"linear",  --interpolation method. Takes string, See just-lean.lua or ease.lua for valid interps. if not using Ease However; "linear" is the only valid interpolation
 "LEG_LEFT", --type (VALID: LEG_LEFT, LEG_RIGHT, ARM_LEFT, ARM_RIGHT) Arm types not implemented yet
 {1,0.5,0.1}, --strength
 torso, --used to grab active head or active lean modelpart rotations for use in its own rotation
 true --enabled
 )
 
---you know the drill
+--Same as left
 local right = jl.influence:new(
 models.model.root.RightLeg, 
 0.5, 
